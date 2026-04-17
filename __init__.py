d994c403ca8ce14cdd0ee4cd59e885b454584c25
igtz-hub <243434193+igtz-hub@users.noreply.github.com>
2026-04-17 15:37:43 -0400

    Register colormaps from colormaps module
    Updated colormap registrations to use imported colormaps.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
-from matplotlib.colors import LinearSegmentedColormap, Normalize
-import matplotlib.cm as cm
+from .colormaps import nanoanalysis_bright, nanoanalysis_muted, nanoanalysis_hazy
 
 plt.register_cmap(name = 'nanoanalysis_bright', cmap=nanoanalysis_bright)
 plt.register_cmap(name = 'nanoanalysis_muted', cmap=nanoanalysis_bright)
